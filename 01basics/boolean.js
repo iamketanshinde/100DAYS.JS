ab8c745fcc7aae5e27bd3e271d0ce56eb6931cbb
Ketan Shinde <ketan_shinde@hotmail.com>
2024-02-16 21:37:09 +0530

    today learned about parse.float & parse.int

--- 01basics/boolean.js
+++ 01basics/boolean.js
@@ -9,3 +9,6 @@ let age2=100;
 //=== check for type and value
 //"10"==10 ans:-true
 //"10"===10 ans:-false
+
+//parse.float('20.3658936');  return 20.3658936 we have to write it in string
+//parse.int('20.3658936'); will return 20
