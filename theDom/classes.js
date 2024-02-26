741c17e67af245200ded3a0b5dc2aba75b08d1ce
Ketan Shinde <ketan_shinde@hotmail.com>
2024-02-26 23:26:11 +0530

    today learned about data attribute

--- theDom/classes.js
+++ theDom/classes.js
@@ -7,3 +7,22 @@ function toggleround(){
     pic.classList.add('round');
 }
 pic.addEventListener('click', toggleround);
+pic.alt='naturefeels';
+pic.width=288;
+console.log(pic.naturalwidth);
+
+
+
+// pic.addEventListener('load',function(){
+//     console.log(pic.naturalwidth);   
+// })
+pic.setAttribute('Alternative','wes is cool');
+console.log(pic.getAttribute('alt'));
+
+
+const custom=document.querySelector('.vogue');
+console.log(custom.dataset);
+
+custom.addEventListener('click',function(){
+alert(`welcome to img section ${custom.dataset.name} ${custom.dataset.last}`)
+});
