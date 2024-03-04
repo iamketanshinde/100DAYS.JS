319d9b3f8a77f66b29b562a7dff325c202ed2cfc
Ketan Shinde <ketan_shinde@hotmail.com>
2024-03-04 22:22:02 +0530

    today learned about capturephase and bubblingphase

--- event/Eventlist.js
+++ event/Eventlist.js
@@ -17,13 +17,32 @@ butts.addEventListener('click',clicked);
 const buybutton=document.querySelectorAll('button.buy');
 
 function handlebuyitem(event){
+    console.log('hy yoou clickedd on button');
+    const button = event.target
+    console.log(button);
+    console.log(event.currentTarget);
  console.log('hey item is collected');
  console.log(event.target.dataset.price);
+//  event.stopPropagation();
 };
  buybutton.forEach(function(buybutton){
     buybutton.addEventListener('click',handlebuyitem)
  });
+ window.addEventListener('click',function(event){
+    console.log('hey you clicked on window');
+    console.log(event.target);
+    console.log(event.type);
+    event.stopPropagation();
+    console.log(event.bubbles);
+    // event.stopPropagation();
+ },{capture:true})
 
+
+  const elph=document.querySelector('.photo');
+  elph.addEventListener('mousemove',function(event){
+    console.log(event.currentTarget);
+    console.log(this);
+  })
 
 // function buyitem(){
 //     console.log('buying item !!');
