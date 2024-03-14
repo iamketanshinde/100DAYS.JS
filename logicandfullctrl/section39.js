0239efedcc564432db07a4d2d6e27fbf14fb14b8
Ketan Shinde <ketan_shinde@hotmail.com>
2024-03-15 00:13:27 +0530

    turtle animating with css and switch case

--- logicandfullctrl/section39.js
+++ logicandfullctrl/section39.js
@@ -1,11 +1,46 @@
 //ternary
-const count=2;
-let word;
+const count=4483;
+// let word;
 
-const sentence=`you have ${count} ${word} in your basket`
+// if(count===1){
+//     word='item'
+// }else{
+//     word='items'
+// };
+const ward=count===1?'item':'items';
 
-if(count===1){
-    word='item'
+const sentence=`you have ${count} ${ward} in your basket`
+
+console.log(sentence);
+
+function truefunct(){
+    console.log('hey adminbar is true');
+}
+const isadmin=true;
+// isadmin?truefunct():null;
+isadmin&&truefunct();
+
+
+
+
+
+//and and trrick
+function check1(){
+    console.log('rennung check pass 1');
+    return true;
+}
+function check2(){
+    console.log('rennung check pass 2');
+    return true;
+}
+function check3(){
+    console.log('rennung check pass 3');
+    return true;
+}
+
+
+if(check1() && check2() && check3()){
+    console.log('all check passed!');
 }else{
-    word='items'
+    console.log('some check failed');
 };
---
+++ logicandfullctrl/section40.js
@@ -0,0 +1,44 @@
+const turtle=document.querySelector('.turt');
+console.log(turtle);
+let x=10;
+let y=10;
+const speed=5;
+let flipped=false;
+let rotate=0;
+
+
+function handlekeydown(event){
+    //if its not an arrow ,we dont care
+if(!event.key.includes('Arrow')){return;}
+switch(event.key)
+{
+    case 'ArrowLeft':
+       x = x - 1;
+       flipped=true;
+       rotate=0;
+        break;
+    case 'ArrowRight':
+        x = x + 1;
+        flipped=false;
+        rotate=0;
+        break;
+    case 'Arrowdown':
+        y = y + 1;
+        rotate=90;
+        break;
+    case 'Arrowup':
+        y = y - 1;
+        rotate=-90;
+        break;
+    default:
+        console.log('its not working');
+}
+turtle.setAttribute('style',
+`
+--rotate:${flipped ? '180deg':'0'}
+--x: ${x*speed}px;
+--rotate:${rotate}px;
+ --y:${y*speed}deg;`
+ );
+}
+window.addEventListener('keydown',handlekeydown);
